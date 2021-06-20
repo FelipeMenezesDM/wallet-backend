9a2331f484691d1cf0066aa83e2fe202e6962300
Felipe Menezes <felipemenezes.web@gmail.com>
2021-06-19 22:12:52 -0300

    Adicionando arquivo exemplo para configuração da conexão com a base de dados.

---
+++ init.example.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Arquivo de inicialização da aplicação.
+ * 
+ * @author    Felipe Menezes <contato@felipemenezes.com.br>
+ * @copyright (c) 2021 Felipe Menezes
+ * @package   Wallet_Backend
+ * @version   1.0.0
+ */
+
+define( "DB_MANAGER", "postgres" );
+define( "DB_NAME", "DB_NAME" );
+define( "DB_USER", "DB_NAME" );
+define( "DB_PASSWORD", "DB_PASSWORD" );
+define( "DB_HOST", "DB_HOST" );
+define( "DB_PORT", 5432 );
